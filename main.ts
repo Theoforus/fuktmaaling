fe0fdafabe1f861549b8247a57ec7c2da44ad1e6
Theoforus <37558526+Theoforus@users.noreply.github.com>
2020-10-18 23:04:52 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,11 @@
 let humidity = 0
 basic.forever(function () {
-    let les_av = 0
     humidity = pins.analogReadPin(AnalogPin.P0)
-    led.plotBarGraph(
-    les_av,
-    1023
-    )
-    if (input.buttonIsPressed(Button.A)) {
-        basic.showNumber(les_av)
+    if (humidity < 150) {
+    	
+    } else if (false) {
+    	
+    } else {
+    	
     }
 })
