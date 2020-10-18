20e23d8c609f6857ecc05a5c4d3ac53fa2e02208
Theoforus <37558526+Theoforus@users.noreply.github.com>
2020-10-18 23:13:22 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,25 @@
 let humidity = 0
+let Pumpe = 0
 basic.forever(function () {
     humidity = pins.analogReadPin(AnalogPin.P0)
-    if (humidity < 150) {
-    	
-    } else if (false) {
+    if (humidity < 300) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # # #
+            `)
+    } else if (humidity < 300) {
     	
     } else {
     	
+    }
+})
+basic.forever(function () {
+    if (humidity < 300) {
+        let Pumpe_på = 0
+        Pumpe = Pumpe_på
+        basic.pause(5000)
     }
 })
